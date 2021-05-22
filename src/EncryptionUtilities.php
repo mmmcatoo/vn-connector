35765f31d086070beb8f8a63cef07351f14d2fbd
mmmcatoo <mmmcatoo@qq.com>
2021-05-22 16:38:29 +0800

    Fix: 更新加密函数算法

--- src/EncryptionUtilities.php
+++ src/EncryptionUtilities.php
@@ -22,7 +22,7 @@ class EncryptionUtilities
     public static function encrypt(string $hash, string $key) {
         $iv = substr(sha1($key), 0, 16);
         $key = substr(hash('sha256', $key), 0, 24);
-        return base64_encode(openssl_encrypt(utf8_encode($hash), 'camellia-256-cbc', $key,OPENSSL_ZERO_PADDING, $iv));
+        return base64_encode(openssl_encrypt(utf8_encode($hash), 'camellia-256-cbc', $key,0, $iv));
     }
 
     /**
@@ -34,6 +34,6 @@ class EncryptionUtilities
     public static function decrypt($hash, string $key) {
         $iv = substr(sha1($key), 0, 16);
         $key = substr(hash('sha256', $key), 0, 24);
-        return openssl_decrypt(base64_decode($hash), 'camellia-256-cbc', $key, OPENSSL_ZERO_PADDING, $iv);
+        return openssl_decrypt(base64_decode($hash), 'camellia-256-cbc', $key, 0, $iv);
     }
 }
